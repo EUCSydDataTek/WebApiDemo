5e8370f8a20eb4786823747ed01c28f0c6170e5f
EgonRasmussen <ecr@eucsyd.dk>
2024-08-26 09:40:05 +0200

    ok

---
+++ SimpleWebApi/Controllers/BlogsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleWebApi.Models;
+
+namespace SimpleWebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BlogsController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<List<Blog>> GetBlogs()
+    {
+        return new List<Blog>
+        {
+            new Blog { BlogId = 1, Url = "https://blog1.com", Rating = 2 },
+            new Blog { BlogId = 2, Url = "https://blog2.com", Rating = 3 },
+            new Blog { BlogId = 3, Url = "https://blog3.com", Rating = 1 },
+            new Blog { BlogId = 4, Url = "https://blog5.com", Rating = 3 }
+        };
+    }
+}
---
+++ SimpleWebApi/Models/Blog.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleWebApi.Models;
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string? Url { get; set; }
+    public int? Rating { get; set; }
+}
